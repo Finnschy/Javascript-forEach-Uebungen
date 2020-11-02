dc4fd3af998a72ebf3cb9382cf43160d26a67757
Finnschy <skylerfearless@yahoo.de>
2020-11-02 16:38:51 +0100

    1_2 durch, weiter mit 1_3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,34 @@
-let getraenke = [
-    "Coca-Cola",
-    "Apfelsaft",
-    "Pepsi",
-    "Traubensaft",
-    "Sprite",
-    "Orangensaft",
-    "Red Bull Energy Drink",
-    "Fanta"
- ]
- 
- function myDrinks() {
-     let getraenkeListe = document.getElementById("getraenkeListe")
-     console.log("Test");
-     console.log(getraenke);
-     getraenke.sort()
-     getraenke.forEach(drink => getraenkeListe.innerHTML += drink + "<br>")
- }
+// Lev1_1_js-vertiefung_array-interation_forEach()
 
- myDrinks()
+// let getraenke = [
+//     "Coca-Cola",
+//     "Apfelsaft",
+//     "Pepsi",
+//     "Traubensaft",
+//     "Sprite",
+//     "Orangensaft",
+//     "Red Bull Energy Drink",
+//     "Fanta"
+//  ]
+ 
+//  function myDrinks() {
+//      let getraenkeListe = document.getElementById("getraenkeListe")
+//      getraenke.sort()
+//      getraenke.forEach(drink => getraenkeListe.innerHTML += drink + "<br>")
+//  }
+//  myDrinks()
+
+
+//  // Lev1_2_js-vertiefung_array-interation_map()
+
+// let upperDrinks = getraenke.map(drinksToUpper);
+// function drinksToUpper(drink) {
+//     return drink.toUpperCase();
+// }
+
+// console.log(upperDrinks);
+
+
+
+// Lev1_3_js-vertiefung_array-interation_map()_sort()
+
