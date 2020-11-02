f5280d4ae8389eead36817d8716f7544dde50bb9
Finnschy <skylerfearless@yahoo.de>
2020-11-02 16:46:05 +0100

    1_3 durch, weiter mit 1_4

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,3 +32,50 @@
 
 // Lev1_3_js-vertiefung_array-interation_map()_sort()
 
+let array = [
+    18,
+    16,
+    80,
+    51,
+    47,
+    38,
+    95,
+    42,
+    68,
+    61,
+    34,
+    51,
+    20,
+    17,
+    56,
+    31,
+    100,
+    6,
+    5,
+    30,
+    74,
+    97,
+    28,
+    99,
+    91,
+    27,
+    73,
+    12,
+    92,
+    6,
+    27,
+    71,
+    26,
+    15,
+    78
+  ];
+
+  let newArray = array.map(rechnen)
+  function rechnen(elt) {
+      return elt * 2
+  }
+  newArray.sort((a, b) => a - b)
+  console.log(newArray);
+
+
+  // Lev1_4_js-vertiefung_array-interation_map()_math.round
